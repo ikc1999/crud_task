87fcd514b5925b7ebaf848f2bea1317c43c1f1ee
Isha <ishachavda1999@gmail.com>
2022-01-19 17:39:31 +0530

    commited

--- connection.php
+++ connection.php
@@ -1,8 +1,4 @@
 <<<<<<< HEAD
 <?php
     $connection  = mysqli_connect('localhost','root','','crud_task') or die('Cannot connect to database');
-=======
-<?php
-    $connection  = mysqli_connect('localhost','root','','crud_task') or die('Cannot connect to database');
->>>>>>> da63d014078e7a7f5bda6e65dd92f61b3432c59f
 ?>
